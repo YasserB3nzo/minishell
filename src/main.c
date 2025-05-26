cfe54fa745f7d8e5a185464c368fc9b16245a815
Anas Chajar <achajar@e2r3p4.1337.ma>
2025-05-26 16:49:39 +0100

    pwd split

--- src/main.c
+++ src/main.c
@@ -1,17 +1,39 @@
+#include "../libft/libft.h"
 #include "../minishell.h"
+#include <readline/history.h>
+#include <readline/readline.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main()
+void	pwd(void)
 {
-    char *command_line;
+	char	*cwd;
 
-    command_line = readline("minishell >");
-    
-    if( parssing(command_line) )
-    {
-        printf("gooooooooood");
-    }
-    else
-        printf("bbbbbbbbbbbbad");
+	cwd = getcwd(NULL, 0);
+	if (cwd)
+	{
+		write(1, cwd, ft_strlen(cwd));
+		write(1, "\n", 1);
+		free(cwd);
+	}
+}
 
-    return 0;
+int	main(void)
+{
+	char	*command_line;
+	char	**comands;
+
+	while (1)
+	{
+		command_line = readline("minishell >");
+		comands = ft_split(command_line, ' ');
+		if (!(ft_strncmp(comands[0], "pwd", ft_strlen(comands[0]))))
+		{
+			pwd();
+		}
+		if (ft_strncmp(comands[0], "exit", ft_strlen(comands[0])))
+		{
+			return (0);
+		}
+	}
 }
